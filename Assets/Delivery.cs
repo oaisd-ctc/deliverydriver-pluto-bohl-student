348dafbb16bda193aceda238c4790da4e9a8dd2d
Pluto <112419543+pluto-bohl-student@users.noreply.github.com>
2023-03-08 14:35:55 -0500

    jjkljkjkljkl

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -10,15 +10,28 @@ public class Delivery : MonoBehaviour
 
     bool hasPackage = false;
 
+    SpriteRenderer spriteRenderer;
+
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
+    
+    [SerializeField] float destroyDelay = 0.5f;
+
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Package") {
+        if (other.tag == "Package" && !hasPackage) {
             Debug.Log("Picked Up A Package.");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
         }
         
         if (other.tag == "Customer" && hasPackage) {
             Debug.Log("Package Delivered.");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -7,13 +7,11 @@ using System;
 public class Driver : MonoBehaviour
 {
     // Start is called before the first frame update
+
     [SerializeField] int steerSpeed = 200;
     [SerializeField] float moveSpeed = 13.50f;
-
-    void Start()
-    {
-        
-    }
+    [SerializeField] int slowSpeed = 10;
+    [SerializeField] float boostSpeed = 17.5f;
 
     // Update is called once per frame
     void Update()
@@ -23,5 +21,22 @@ public class Driver : MonoBehaviour
 
         transform.Translate(0, accelerateAmount, 0);
         transform.Rotate(0, 0, steerAmount);
+    }
+
+    int numberBoosts = 0;
+
+    private void OnTriggerEnter2D(Collider2D bump) {
+        if (bump.tag == "Boost" && numberBoosts == 0)
+        {
+            moveSpeed = boostSpeed;
+        } else if (bump.tag == "Boost" && numberBoosts >= 1 && numberBoosts <= 5)
+        {
+            numberBoosts++;
+            moveSpeed = moveSpeed + 2.5f;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        moveSpeed = slowSpeed;
     }
 }
